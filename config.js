7cb3e45027ed7b86ceb5ccd7c035bd66b58fca2f
gfk22 <82573382+gfk22@users.noreply.github.com>
2023-03-08 08:49:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-  token: "ODkzMDcyNjg1NjYyMzM5MTEy.GmqPYr.RTOCNZmqZXC6tIePwno8xXsVLU4bsc6erG3B0U",
+  token: "isi token",
   channelId: "1061681370943979530",
   timezone: "Asia/Jakarta",
   projectName: "Mooh",
